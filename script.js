09786bbf1f074bb28955b988e2e7db480eedd227
Justin Skiba <justinskiba3@gmail.com>
2020-02-12 21:41:15 -0800

    commiting changes

---
+++ script.js
@@ -0,0 +1,56 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    }
+        
+    function showResults(questions, quizContainer, restultsContainer){
+
+    }
+    function showQuestions(questions, quizContainer) {
+        var output = [];
+        var answers;
+
+        for(var i=0; i<questions.length; i++){
+            answers =[];
+            for(letter in questions[i].answers){
+                answers.push(
+                    '<label>'
+                        +'<input type="radio" name ="question'+i+'" value="'+letter+'">'
+                        + letter + ':'
+                        +questions[i].answers[letter]
+                    +'</label>'  
+            );  
+            }
+        output.push(
+            '<div class="qwestion">' + questions[i].question + '</div>'
+            + '<div class ="answers">' + answers.join('')+'</div>'        
+            );
+        }
+    };
+
+    submitButton.onclick = function(){
+        showResults(questions, quizContainer, resultsContainer);
+    }
+
+var myQuestions = [
+    {
+              question: "Commonly used data types DO NOT include:",
+              answers: {
+                  a: 'strings',
+                  b: 'booleans',
+                  c: 'alerts', 
+                  d: 'numbers' 
+            },
+            correctAnswer: 'c'
+            {
+              question: "The condition in an if / else statement is enclosed within ____.",
+              answers: {
+                  a: 'quotes',
+                  b: 'curly brackets',
+                  c: 'parentheses',
+                  d: 'square brackets'
+              },
+              correctAnswer: 'parentheses'
+            }
+          ];
+        
+          
+

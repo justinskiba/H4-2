af48b3fdc7d32c662983ffc6bbe98a9711bc906c
Justin Skiba <justinskiba3@gmail.com>
2020-02-15 13:46:01 -0800

    commiting changes

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-    }
-        
-    function showResults(questions, quizContainer, restultsContainer){
-
-    }
-    function showQuestions(questions, quizContainer) {
-        var output = [];
-        var answers;
-
-        for(var i=0; i<questions.length; i++){
-            answers =[];
-            for(letter in questions[i].answers){
-                answers.push(
-                    '<label>'
-                        +'<input type="radio" name ="question'+i+'" value="'+letter+'">'
-                        + letter + ':'
-                        +questions[i].answers[letter]
-                    +'</label>'  
-            );  
-            }
-        output.push(
-            '<div class="qwestion">' + questions[i].question + '</div>'
-            + '<div class ="answers">' + answers.join('')+'</div>'        
-            );
-        }
-    };
-
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
 var myQuestions = [
-    {
-              question: "Commonly used data types DO NOT include:",
-              answers: {
-                  a: 'strings',
-                  b: 'booleans',
-                  c: 'alerts', 
-                  d: 'numbers' 
-            },
-            correctAnswer: 'c'
-            {
-              question: "The condition in an if / else statement is enclosed within ____.",
-              answers: {
-                  a: 'quotes',
-                  b: 'curly brackets',
-                  c: 'parentheses',
-                  d: 'square brackets'
-              },
-              correctAnswer: 'parentheses'
-            }
-          ];
-        
-          
+  {
+    question: "Commonly used data types DO NOT include:",
+    answers: {
+      a: "strings",
+      b: "booleans",
+      c: "alerts",
+      d: "numbers"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within ____.",
+    answers: {
+      a: "quotes",
+      b: "curly brackets",
+      c: "parentheses",
+      d: "square brackets"
+    },
+    correctAnswer: "parentheses"
+  },
 
+  function generateQuiz(
+    questions,
+    quizContainer,
+    resultsContainer,
+    submitButton
+  ) {
+    function showResults(questions, quizContainer, restultsContainer) {}
+    function showQuestions(questions, quizContainer) {
+      var output = [];
+      var answers;
+
+      for (var i = 0; i < questions.length; i++) {
+        answers = [];
+        for (letter in questions[i].answers) {
+          answers.push(
+            "<label>" +
+              '<input type="radio" name ="question' +
+              i +
+              '" value="' +
+              letter +
+              '">' +
+              letter +
+              ":" +
+              questions[i].answers[letter] +
+              "</label>"
+          );
+        }
+        output.push(
+          '<div class="question">' +
+            questions[i].question +
+            "</div>" +
+            '<div class ="answers">' +
+            answers.join("") +
+            "</div>"
+        );
+      }
+      quizContainer.innerHTML = output.join("");
+    }
+
+    submitButton.onclick = function() {
+      showResults(questions, quizContainer, resultsContainer);
+    };
+  }
+];
